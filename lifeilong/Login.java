128a0e871788c3e57c5c754a7c939aad2a48e164
lifeilongAnddove <103204114+lifeilongAnddove@users.noreply.github.com>
2022-07-29 15:58:53 +0800

    Update Login.java

--- lifeilong/Login.java
+++ lifeilong/Login.java
@@ -6,6 +6,7 @@ import java.sql.*;
 import java.util.Scanner;
 public class Login {
     public static void main(String[] args) {
+ System.out.println("lifeilong");
         Scanner sc = new Scanner(System.in);
         System.out.println("请输入用户名");
         String username = sc.next();
