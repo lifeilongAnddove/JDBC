b4c8d1ee469c52bc39be169b9c041c1ab1f9cae0
lifeilong <lifeilong@123.com>
2022-07-29 10:29:00 +0800

    hot-fix commit2

--- lifeilong/UpdataSQL.java
+++ lifeilong/UpdataSQL.java
@@ -8,6 +8,7 @@ import java.sql.Statement;
 public class UpdataSQL {
     public static void main(String[] args) {
         System.out.println("hello lifeilong");
+        System.out.println("hello lifeilong11111");
         Statement stat=null;
         Connection conn=null;
         String sql ="update user set username='张三' where id=1";
